535311fe81728b7b4eed7e6caa20b0275412fb3f
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-13 13:28:21 +0100

    initial site and add frist step for mail

---
+++ asset/js/main.js
@@ -0,0 +1,20 @@
+/* Mail
+ Chiedi all’utente la sua email,
+ controlla che sia nella lista di chi può accedere, 
+ stampa un messaggio appropriato sull’esito del controllo.
+Mail Bonus
+ Usiamo un input e un bottone per inserire la mail 
+ e poi mostriamo i risultati in pagina. */
+
+
+ //steps
+ //Ask user'email
+ const buttonEmail = document.getElementById('button_email')
+ buttonEmail.addEventListener ('click', function () {
+    const userEmail = document.getElementById("user_email").value
+    const emailValue = userEmail
+    console.log(userEmail);
+})
+ //check if user'email is into whaitlist
+
+//print a message for the outcome of the check
